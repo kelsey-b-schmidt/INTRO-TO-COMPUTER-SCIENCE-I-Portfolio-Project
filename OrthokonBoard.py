a3f6ab215f0af2de3c5bbf3e2dee8b8dc59d109a
schmkelsosu <schmkels@oregonstate.edu>
2021-05-20 12:57:56 -0500

    initial class creation, docstring, methods

--- OrthokonBoard.py
+++ OrthokonBoard.py
@@ -1,4 +1,75 @@
 # Author: Kelsey Schmidt
 # Date: 5-20-21
-# Description:  # The "Description" comment should be a high-level description
-                # of what the code in the file does.
+# Description:  # Creates a class called OrthokonBoard that represents the board
+                # for a two-player game that is played on a 4x4 grid.
+                # This class does not do everything needed to play a game
+                # - it's just responsible for handling the rules concerning the game board.
+                # The board starts with four red pieces on row 0 and four yellow pieces on row 3.
+                # A valid move consists of a player moving one of their pieces orthogonally or diagonally
+                # as far as it can go until it hits another piece or the edge of the board
+                # (it must move at least one space).
+                # After the piece stops, any opponent pieces on orthogonally adjacent squares
+                # are flipped over to its color.
+                # The OrthokonBoard class doesn't keep track of whose turn it is,
+                # so it will allow multiple moves by the same player consecutively.
+                # A player wins upon making a move that either flips over
+                # the remaining opponent pieces or leaves the opponent without a move.
+
+class OrthokonBoard:
+    """
+    Creates a class called OrthokonBoard that represents the board
+    for a two-player game that is played on a 4x4 grid.
+    This class does not do everything needed to play a game
+    - it's just responsible for handling the rules concerning the game board.
+    The board starts with four Red pieces (R) on row 0 and four Yellow pieces (Y) on row 3.
+    A valid move consists of a player moving one of their pieces orthogonally or diagonally
+    as far as it can go until it hits another piece or the edge of the board
+    (it must move at least one space).
+    After the piece stops, any opponent pieces on orthogonally adjacent squares
+    are flipped over to its color.
+    The OrthokonBoard class doesn't keep track of whose turn it is,
+    so it will allow multiple moves by the same player consecutively.
+    A player wins upon making a move that either flips over
+    the remaining opponent pieces or leaves the opponent without a move.
+    The class has private data members for:
+            1. a representation of the board (4 lists within a list)
+            2. the current state, which holds one of the three following values:
+                "RED_WON", "YELLOW_WON", or "UNFINISHED"
+    The class includes the following methods:
+        1. An init method that takes no parameters,
+            initializes the starting board positions,
+            and initializes the current state to "UNFINISHED"
+        2. A method named print_board that prints out the board.
+        3. A get method named get_current_state, which returns the current state.
+        4. A method named make_move that takes four parameters (in the specific order listed):
+                A. The row of the piece being moved
+                B. The column of the piece being moved
+                C. The row of the intended position that piece will be moved to
+                D. The column f the intended position that piece will be moved to
+        If the game has already been won, or if the move is not valid,
+        make_move will return False.
+        Otherwise, it will record the move, update the board,
+        update the current state if the move caused a win, and return True.
+    """
+
+    def __init__(self):
+        self._board = [["Y","Y","Y","Y"],[".",".",".","."],[".",".",".","."],["R","R","R","R"]]
+        self._current_state = "UNFINISHED"  # the default state of the game with no moves made is "UNFINISHED"
+
+    def get_current_state(self):        # get method to get the current game state (Who won?)
+        return self._current_state      # returns "RED_WON", "YELLOW_WON", or "UNFINISHED"
+
+    def print_board(self):
+        print("_" * 11)
+        for line in self._board:
+            print("|", *line[:], "|")
+        print("‾" * 11)
+
+    def make_move(self, piece_row, piece_column, position_row, position_column):    # method to make each move
+        return True
+
+
+
+board = OrthokonBoard()
+board.print_board()
+print(board.get_current_state())
